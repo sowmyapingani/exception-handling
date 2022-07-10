d8a570411933061bd8208233d7bbe73fbd0cac48
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-10 12:57:52 +0530

    added:email

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 
 public class exceptionHandling {
-        public static void firstName() {
+    public static void firstname(){
             String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter First Name:");
@@ -13,14 +13,12 @@ public class exceptionHandling {
             Pattern pattern = Pattern.compile(firstNamePattern);
             Matcher matcher = pattern.matcher(firstName);
             boolean result = matcher.matches();
-            if(result == true){
+            if (result == true) {
                 System.out.println("First Name is valid");
-            }
-            else
+            } else
                 System.out.println("First Name is Invalid");
         }
-        public static void lastName() {
-
+        public static void lastName () {
             String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter Last Name:");
@@ -28,18 +26,30 @@ public class exceptionHandling {
             Pattern pattern = Pattern.compile(lastNamePattern);
             Matcher matcher = pattern.matcher(lastName);
             boolean result = matcher.matches();
-
-
-            if(result == true)
+            if (result == true)
                 System.out.println("Last Name is Valid");
             else
                 System.out.println("Last Name is Invalid");
         }
-        public static void main(String[] args) {
+        public static void email () {
+            String emailPattern = "[a-zA-Z0-9]+[.+-]{0,1}[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.a-z]{0,4}";
+            System.out.println("Enter your Email:");
+            Scanner scanner = new Scanner(System.in);
+            String email = scanner.next();
+            Pattern pattern = Pattern.compile(emailPattern);
+            Matcher matcher = pattern.matcher(email);
+            boolean result = matcher.matches();
+
+            if (result == true) {
+                System.out.println("true");
+            } else {
+                System.out.println("Email is not valid");
+            }
+        }
+        public static void main (String[]args){
             System.out.println("Welcome to Exception Handling");
-            firstName();//UC1
+            firstname();//UC1
             lastName();//UC2
+            email();//UC3
         }
     }
-
-
