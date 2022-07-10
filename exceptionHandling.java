6f767cfcedc031f98a9bef8f5d10191913cf3731
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-10 12:52:34 +0530

    added:firstname

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -5,23 +5,41 @@ import java.util.regex.Pattern;
 
 
 public class exceptionHandling {
-    public static void firstName() {
-        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Name:");
-        String firstName = scanner.next();
-        Pattern pattern = Pattern.compile(firstNamePattern);
-        Matcher matcher = pattern.matcher(firstName);
-        boolean result = matcher.matches();
-        if (result == true) {
-            System.out.println("First Name is valid");
-        } else
-            System.out.println("First Name is Invalid");
+        public static void firstName() {
+            String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter First Name:");
+            String firstName = scanner.next();
+            Pattern pattern = Pattern.compile(firstNamePattern);
+            Matcher matcher = pattern.matcher(firstName);
+            boolean result = matcher.matches();
+            if(result == true){
+                System.out.println("First Name is valid");
+            }
+            else
+                System.out.println("First Name is Invalid");
+        }
+        public static void lastName() {
+
+            String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Last Name:");
+            String lastName = scanner.next();
+            Pattern pattern = Pattern.compile(lastNamePattern);
+            Matcher matcher = pattern.matcher(lastName);
+            boolean result = matcher.matches();
+
+
+            if(result == true)
+                System.out.println("Last Name is Valid");
+            else
+                System.out.println("Last Name is Invalid");
+        }
+        public static void main(String[] args) {
+            System.out.println("Welcome to Exception Handling");
+            firstName();//UC1
+            lastName();//UC2
+        }
     }
 
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Exception Handling");
-        firstName();//UC1
-    }
-}
