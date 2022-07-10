25ccfd44c6ff834479302bd3c43f0fd87472e31c
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-10 12:45:26 +0530

    added:firstname

---
+++ exceptionHandling.java
@@ -0,0 +1,27 @@
+package com.bridge.labzs;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class exceptionHandling {
+    public static void firstName() {
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+        if (result == true) {
+            System.out.println("First Name is valid");
+        } else
+            System.out.println("First Name is Invalid");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Exception Handling");
+        firstName();//UC1
+    }
+}
